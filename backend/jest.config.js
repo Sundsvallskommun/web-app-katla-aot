e1c42cec542ac6132c7882b2a99ac7cd218584b3
Max Eriksson <max.eriksson@devize.se>
2026-08-10 14:56:52 +0200

    fix: close dependency and clean-checkout gaps

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,6 +4,7 @@ const { compilerOptions } = require('./tsconfig.json');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/jest.set-env.js'],
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
